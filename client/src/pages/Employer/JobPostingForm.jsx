65b9c6a276d039eaf1458ae0af50ad8b405e901a
WellsJhones <wellsjhones@gmail.com>
2025-08-22 16:42:57 -0300

    added new folder structures and empt files

---
+++ client/src/pages/Employer/JobPostingForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function JobPostingForm() {
+  return (
+    <div>
+         <h1 className="text-3xl font-bold">Job Posting Form</h1>
+    </div>
+  )
+}
+
+export default JobPostingForm
