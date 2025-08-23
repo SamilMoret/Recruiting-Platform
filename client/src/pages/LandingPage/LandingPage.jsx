dab4c3621004b0860c2a1f1ff1fc540760971b72
WellsJhones <wellsjhones@gmail.com>
2025-08-23 10:35:21 -0300

    move folder to src and add more hardcoded data for testing

--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import Features from './components/Features'
+import Analytics from './components/Analytics'
 
 const LandingPage = () => {
   return (
-      <div className='min-h-screen'>
+      <div className='min-h-screen mb-[100vh]'>
         <Header />
         <Hero />
+        <Features />
+        <Analytics />
       </div>
   )
 }
