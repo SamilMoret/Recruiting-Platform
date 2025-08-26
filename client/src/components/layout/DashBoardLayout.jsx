fd06b3f4e344e1a0c997d173e5c5d9694140345f
WellsJhones <wellsjhones@gmail.com>
2025-08-26 16:31:23 -0300

    add butons sidear

--- client/src/components/layout/DashBoardLayout.jsx
+++ client/src/components/layout/DashBoardLayout.jsx
@@ -17,8 +17,21 @@ const NavigationItem = ({
     onClick,
     isCollapsed,
 }) =>{
-    const Icon = item.Icon;
-    return <>nav</>
+
+    const Icon = item.icon;
+    return <button
+        onClick={() => onClick(item.id)}
+        className={`w-full flex items-center px-3 py-2.5 text-sm font-medium rounded-lg ${
+            isActive 
+            ? "bg-blue-50 text-blue-700 shadow-sm shadow-blue-50" 
+            : "text-gray-700 hover:bg-gray-50 hover:text-gray-900"
+        }`}
+        >       
+           <Icon
+              className={`w-5 h-5 flex-shrink-0 ${
+                isActive ? "text-blue-600" : "text-gray-500"}`} 
+           /> {!isCollapsed && <span className='ml-3 truncate'>{item.name}</span>}
+        </button>
 }
 
 const DashBoardLayout = ({activeMenu}) => {
