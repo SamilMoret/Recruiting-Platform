cf18e1231ff302fb524c36f0430c437ed5319037
WellsJhones <wellsjhones@gmail.com>
2025-09-03 08:00:34 -0300

    prevent user to acess not authorized

--- client/src/routes/ProtectedRoute.jsx
+++ client/src/routes/ProtectedRoute.jsx
@@ -1,12 +1,22 @@
 import React from "react";
-import{Navigate, Outlet, useLocation} from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
-const  ProtectedRoute = ({requiredRole}) => {
+const ProtectedRoute = ({ requiredRole }) => {
+  const { user } = useAuth();
+  const location = useLocation();
 
-    // will implement later
-    return (
-        <Outlet />
-    );
+  // If not logged in, redirect to login
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  // If role is required and user does not have it, redirect to home
+  if (requiredRole && user.role !== requiredRole) {
+    return <Navigate to="/" replace />;
+  }
+
+  return <Outlet />;
 };
 
 export default ProtectedRoute;
